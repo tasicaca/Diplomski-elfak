dff6ca931610e1b97d96762bb1f760cd6acdf9a2
Aleksandar Tasić <96747833+tasicaca@users.noreply.github.com>
2023-12-26 03:00:15 +0100

    Add files via upload

---
+++ 26122023- DETEKTUJEDONJIDEOSAMO.py
@@ -0,0 +1,192 @@
+import cv2 as cv
+import numpy as np
+import time
+
+# Constants
+whT = 320
+confThreshold = 0.5
+nmsThreshold = 0.3
+
+# LOAD YOLO MODEL
+classesFile = 'coco.names'
+classNames = []
+with open(classesFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+modelConfiguration = "yolov3.cfg"
+modelWeights = "yolov3.weights"
+net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+# Define line coordinates and sensitivity (adjust as needed)
+line_coordinates = [(100, 300), (500, 300)]
+sensitivity = 1
+line_thickness = 2
+
+# Define the time threshold for showing the red light
+red_light_threshold = 0
+
+# Adjusted sensitivity for object leaving the road
+leave_road_threshold = 0
+
+# Kalman Filter Initialization
+kalman = cv.KalmanFilter(4, 2)
+kalman.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], dtype=np.float32)
+kalman.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], dtype=np.float32)
+
+# Region of Interest (ROI) for road detection
+roi_coordinates = [(0, 300), (640, 480)]
+
+def find_objects(outputs, image):
+    hT, wT, cT = image.shape
+    bounding_boxes = []
+    class_ids = []
+    confidences = []
+
+    target_classes = ["car", "motorbike", "bus", "truck"]
+
+    for output in outputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+
+            if confidence > confThreshold and classNames[class_id] in target_classes:
+                w, h = int(detection[2] * wT), int(detection[3] * hT)
+                x, y = int((detection[0] * wT) - w / 2), int((detection[1] * hT) - h / 2)
+                bounding_boxes.append([x, y, w, h])
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+
+    indices = cv.dnn.NMSBoxes(bounding_boxes, confidences, confThreshold, nmsThreshold)
+
+    objects = []
+    for i in range(len(bounding_boxes)):
+        if i in indices:
+            x, y, w, h = bounding_boxes[i]
+            objects.append((x, y, w, h, True))
+            cv.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Green rectangles
+            cv.putText(image, f'{classNames[class_ids[i]].upper()} {int(confidences[i] * 100)}%',
+                       (x, y - 10), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)  # Green text
+
+    return objects
+
+def pronadji_put(slika, objekti):
+    # Convert the frame to grayscale
+    siva_slika = cv.cvtColor(slika, cv.COLOR_BGR2GRAY)
+
+    # Use adaptive thresholding
+    adaptive_threshold = cv.adaptiveThreshold(siva_slika, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+
+    # Set the region inside the adaptive threshold as the road
+    maska_puta = adaptive_threshold.copy()
+
+    # Use region of interest (ROI) for road detection
+    roi_vertices = np.array([[(50, 400), (300, 100), (500, 100), (700, 400)]], dtype=np.int32)
+    maska_puta = cv.fillPoly(maska_puta, roi_vertices, 0)
+
+    konture, _ = cv.findContours(maska_puta, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    konture_puta = [kontura for kontura in konture if cv.contourArea(kontura) > 1000]
+
+    # Identifikacija bele linije s jedne strane puta
+    for kontura in konture_puta:
+        aproksimacija = cv.approxPolyDP(kontura, 0.02 * cv.arcLength(kontura, True), True)
+        if len(aproksimacija) == 2:
+            cv.drawContours(slika, [kontura], -1, (255, 255, 255), 2)  # White color for the line
+        else:
+            cv.drawContours(slika, [kontura], -1, (128, 0, 128), 2)  # Purple color for road edges
+
+    return konture_puta, maska_puta
+
+#Replace the existing pronadji_put function in your code with this improved version. This code includes adaptive thresholding and a region of interest (ROI) for road detection. Adjust the roi_vertices as needed to fit your specific road region.
+
+prelazeci_objekti = []
+
+cap = cv.VideoCapture('madjarskagranicestaze.mp4')
+
+while True:
+    current_time = time.time()  # Get the current time
+
+    success, frame = cap.read()
+    if not success:
+        break
+
+    blob = cv.dnn.blobFromImage(frame, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+    net.setInput(blob)
+    layer_names = net.getLayerNames()
+    output_names = [(layer_names[i - 1]) for i in net.getUnconnectedOutLayers()]
+    outputs = net.forward(output_names)
+
+    objects = find_objects(outputs, frame)
+    road_contours, road_mask = pronadji_put(frame, objects)
+
+    # Update prelazeci_objekti based on whether an object intersects with the road contours
+
+    for i, obj in enumerate(objects):
+        x, y, w, h, _ = obj
+        bottom_center_x = x + w // 2
+        bottom_center_y = y + h
+        bottom_center = (bottom_center_x, bottom_center_y)
+
+        # Increase the strictness for leaving the road detection
+        on_road = False
+        for contour in road_contours:
+            # Consider a smaller region around the bottom center
+            region_radius = 5
+            in_region = (
+                    bottom_center_x - region_radius < contour[:, 0, 0].max() < bottom_center_x + region_radius
+                    and bottom_center_y - region_radius < contour[:, 0, 1].min() < bottom_center_y + region_radius
+            )
+
+            if in_region:
+                on_road = True
+                break
+
+        # Save the frame when an object leaves the road
+        if not on_road:
+            cv.imwrite(f'frame_{int(current_time)}.png', frame)
+
+        objects[i] = obj[:4] + (on_road,)
+
+        # Save the frame when an object leaves the road
+        if not on_road:
+            cv.imwrite(f'frame_{int(current_time)}.png', frame)
+
+        objects[i] = obj[:4] + (on_road,)
+
+    prelazeci_objekti = [i for i, obj in enumerate(objects) if not obj[-1]]
+
+    for object_index in prelazeci_objekti:
+        x, y, w, h, _ = objects[object_index]
+        cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)  # Yellow rectangles
+
+    # Display red bar at the top of the screen when objects are detected outside the road
+    if prelazeci_objekti:
+        frame[:50, :] = [0, 0, 255]  # Red color
+    else:
+        frame[:50, :] = [0, 0, 0]  # Set to black if no objects are detected
+
+    # Detekcija ivica puta i obelezavanje puta bojom
+    hsv_slika = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    # Definisanje opsega boja za detekciju sive boje puta
+    donja_siva = np.array([10, 10, 60], dtype=np.uint8)
+    gornja_siva = np.array([159, 85, 159], dtype=np.uint8)
+
+    # Filtriranje slike da se dobije samo siva boja puta
+    maska_sive = cv.inRange(hsv_slika, donja_siva, gornja_siva)
+
+    # Neka boja za obelezavanje puta (npr. plava)
+    boja_puta = (255, 0, 0)
+
+    # Oznaci put na frejmu
+    frame[maska_sive == 255] = boja_puta
+
+    cv.imshow('Frame', frame)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
